666deccc7c1206b5bcd0fa188ba7a4697248df6c
yuandapeng <18852369945@163.com>
2026-04-23 23:44:23 +0800

    feat: 添加自定义HTTP头部配置以优化静态资源缓存

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.BASE_URL,
+  async headers() {
+    return [
+      // 静态资源：自动缓存（Next.js 默认最优）
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
